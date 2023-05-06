b15f1a4cc13f9d728c1f335b7fbde2cd0abb5174
Matias Rios <riosmatias2505@gmail.com>
2023-05-06 13:53:48 -0300

    Carga Repositorio

---
+++ main.py
@@ -0,0 +1,30 @@
+
+if __name__ == "__main__":
+    from classRegistro import Registro
+    from menu import menu
+    import csv
+
+    archivo = open('timpo.csv')
+    reader = csv.reader(archivo,delimiter=';')
+    next(reader)
+    registros_mensuales = []
+    dias_en_mes = 30
+    horas_en_dia = 24
+
+    for dia in range(dias_en_mes):
+        registros_diarios = []
+        for hora in range(horas_en_dia):  
+            registros_diarios.append(None)
+        registros_mensuales.append(registros_diarios)
+
+    for fila in reader:
+        print(fila)
+        row = fila[2].split(',')
+        temp = int(row[0])
+        hum = int(row[1])
+        presion = int(row[2])
+        registro = Registro(temp,hum,presion)
+        registros_mensuales[int(fila[0])][int(fila[1])] = registro
+        
+    menu(registros_mensuales)
+    
